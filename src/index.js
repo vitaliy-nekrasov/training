f355beef046738c5a2e9846662cf3ff451132d5d
Vitaliy7171 <vitaliynekrasov1@gmail.com>
2022-10-06 11:56:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-const arrays = [1, [[2], [3]], [[[[4]]]]];
+const API_KEY = '1c308592d719c7668d47adc63622f209';
+const optionsTrending = `trending/movie/week`;
+const BASE_URL = `https://api.themoviedb.org/3/${optionsTrending}?api_key=${API_KEY}`;
 
-console.log(arrays.flatMap(array => array));
+const result = fetch(BASE_URL)
+  .then(response => {
+    return response.json();
+  })
+  .then(({ results }) => {
+    return results;
+  });
+
+const imgEl = document.querySelector('img');
+const nameEl = document.querySelector('.name');
+const filmEl = document.querySelector('.film');
+const numberEl = document.querySelector('.number');
+
+result
+  .then(result => {
+    const id = result[0].id;
+    return id;
+  })
+  .then(id => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        imgEl.setAttribute('src', `https://image.tmdb.org/t/p/w400${data.poster_path}`);
+        nameEl.textContent = `${data.original_title}`;
+        const genres = data.genres;
+        const array = [];
+        genres.forEach(element => {
+          array.push(element.name);
+        });
+        const string = array.join(', ');
+        const year = data.release_date;
+        filmEl.textContent = string + ' ' + year.slice(0, 4);
+        const vote = data.vote_average;
+        numberEl.textContent = vote.toFixed(1);
+      });
+  });
